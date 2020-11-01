5d38fbe00b09992355cdcddf22dcfd6cb0a6f46e
Amrita Dutta <amrita.dutta@oracle.com>
2020-11-01 12:13:01 -0800

    Additional comments + better variable naming

--- RodCuttingRecursive.java
+++ RodCuttingRecursive.java
@@ -17,13 +17,16 @@ class RodCuttingRecursive{
 		System.out.println("Maximum profit for rodLength: " + rodLength + " and prices: " + Arrays.toString(prices) + " is: " + maxProfit);
 	}
 
-	static int rodCutting(int[] prices, int rodLength){
+	static int rodCutting(int[] prices, int remainingRodLength){
 		int maxProfit = 0;
 
 		// iterate over the choices to make a cut
-		// kIndex ==  rodLength means no cut!
-		for(int kIndex = 1; kIndex <= rodLength; kIndex++) { // cutLength = 1; cutLength = 2; 
-			maxProfit = Math.max(maxProfit, prices[kIndex-1] + rodCutting(prices, rodLength-kIndex));
+		// pieceLength ==  rodLength means no cut!
+		for(int pieceLength = 1; pieceLength <= remainingRodLength; pieceLength++) { // cutLength = 1; cutLength = 2; 
+			// price of pieceLength == 1 is given by prices[0] 
+			// price of pieceLength == x is given by prices[x-1] 
+			// this is due to array indices starting from 0 in Java
+			maxProfit = Math.max(maxProfit, prices[pieceLength-1] + rodCutting(prices, remainingRodLength-pieceLength));
 		}
 
 		return maxProfit;
